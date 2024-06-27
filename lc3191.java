beff16011ce0cc1dedd3c1576a5ebad4b4bce280
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-06-27 17:48:06 +0500

    Added 3 more optimal solutions to LeetCode problem lc3191.java

--- lc3191.java
+++ lc3191.java
@@ -33,6 +33,76 @@
 // 3 <= nums.length <= 105
 // 0 <= nums[i] <= 1
 
+
+// Runtime: 5ms solution (beats 100%)
+// Memory: 57.10MB (beats 85.10%)
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0, len = nums.length - 3, x = 0, y = 0, z = 0;
+        x = nums[0];
+        y = nums[1];
+        z = nums[2];
+        for (int m = 0; m < len; m++) {
+            if (x == 0) {
+                count++;
+                x = 1;
+                y = y == 0 ? 1 : 0;
+                z = z == 0 ? 1 : 0;
+                x = y;
+                y = z;
+                z = nums[m+3];
+            } else {
+                x = y;
+                y = z;
+                z = nums[m+3];
+            }
+        }
+        if (x == 0 && y == 0 && z == 0) return ++count;
+        if (x == 1 && y == 1 && z == 1) return count;
+        return -1;
+    }
+}
+
+// Runtime: 6ms solution (beats 99.99%)
+// Memory: 56.93MB (beats 92.17%)
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0, len = nums.length - 2;
+        for (int m = 0; m < len; m++) {
+            if (nums[m] == 0) {
+                count++;
+                nums[m] = 1;
+                if (nums[m + 1] == 0) nums[m + 1] = 1;
+                else nums[m+1] = 0;
+                if (nums[m + 2] == 0) nums[m + 2] = 1;
+                else nums[m+2] = 0;
+            }
+        }
+        if (nums[len] == 0 || nums[len + 1] == 0) return -1;
+        else return count;
+    }
+}
+
+// Runtime: 6ms solution (beats 99.99%)
+// Memory: 57.18MB (beats 72.36%)
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0, len = nums.length - 2;
+        for (int m = 0; m < len; m++) {
+            if (nums[m] == 0) {
+                count++;
+                nums[m] = 1;
+                nums[m + 1] = nums[m + 1] == 0 ? 1 : 0;
+                nums[m + 2] = nums[m + 2] == 0 ? 1 : 0;
+            }
+        }
+        if (nums[len] == 0 || nums[len + 1] == 0) return -1;
+        else return count;
+    }
+}
+
+// Runtime: 8ms solution (beats 36.20%)
+// Memory: 57.23MB (beats 62.07%)
 class lc3191 {
     public int minOperations(int[] nums) {
         int count = 0;
