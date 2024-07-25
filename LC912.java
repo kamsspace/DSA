5fe40f23374434ad2d839b758b78481fbd8a3dba
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-07-25 14:51:15 +0500

    Update LC912.java | Added the second solution 26ms | 25Jul24 - Sort an Array

--- LC912.java
+++ LC912.java
@@ -1,4 +1,6 @@
 // problem: sort an array
+
+// 1.
 // runtime: 38ms, beats 31.01%
 // memory: 55.61MB, beats 67.65%
 class Solution {
@@ -33,6 +35,57 @@ class Solution {
     }
     public int[] sortArray(int[] nums) {
         heapSort(nums);
+        return nums;
+    }
+}
+
+// 2.
+// runtime: 26ms, beats 65.17%
+// memory: 56.05MB, beats 53.26%
+class Solution {
+    public static void mergeSort(int[] check, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            mergeSort(check, l, m);
+            mergeSort(check, m + 1, r);
+            merge(check, l, m, r);
+        }
+    }
+    public static void merge(int[] check, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        System.arraycopy(check, l, L, 0, n1);
+        System.arraycopy(check, m + 1, R, 0, n2);
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                check[k] = L[i];
+                i++;
+            } else {
+                check[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            check[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            check[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
 }
