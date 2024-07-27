a8eaf979a9c419d32915e5dd97a84290df897544
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-07-27 14:04:02 +0500

    Solved medium level problem LC2AddTwoNumbers.java on LeetCode | 27Jul24

---
+++ LC2AddTwoNumbers.java
@@ -0,0 +1,38 @@
+// difficulty: medium
+// problem: 2. Add Two Numbers
+
+// runtime: 1ms, beats 100%
+// memory: 44.34MB, beats 45.97%
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode(0);
+        ListNode current = l3;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int a = (l1 != null) ? l1.val : 0;
+            int b = (l2 != null) ? l2.val : 0;
+            int sum = carry + a + b;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0) current.next = new ListNode(carry);
+        return l3.next;
+    }
+}
