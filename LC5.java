e38552b8c027871d9c609e689afd7469885e14e7
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-07-29 20:00:54 +0500

    Updated LC5.java with new solution

--- LC5.java
+++ LC5.java
@@ -34,3 +34,58 @@ class Solution {
         return true;
     }
 }
+
+
+// 2. 
+// runtime: 11ms, beats 95%
+// memory: 43.11MB, beats 67%
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        
+        String t = prepro(s);
+        int n = t.length();
+        int[] p = new int[n];
+        int center = 0, right = 0;
+        
+        for (int i = 1; i < n - 1; i++) {
+            int mirror = 2 * center - i;
+            
+            if (i < right) {
+                p[i] = Math.min(right - i, p[mirror]);
+            }
+            
+            while (t.charAt(i + (1 + p[i])) == t.charAt(i - (1 + p[i]))) {
+                p[i]++;
+            }
+            
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+        }
+        
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIndex = i;
+            }
+        }
+        
+        int start = (centerIndex - maxLen) / 2;
+        return s.substring(start, start + maxLen);
+    }
+
+    private String prepro(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('^');
+        for (int i = 0; i < s.length(); i++) {
+            sb.append('#');
+            sb.append(s.charAt(i));
+        }
+        sb.append("#$");
+        return sb.toString();
+    }
+}
