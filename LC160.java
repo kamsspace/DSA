fc3851e1418101effe07ce9ed08ee614469d4f8f
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-08-10 18:03:55 +0500

    Updated with more solutions for problem LC160.java

--- LC160.java
+++ LC160.java
@@ -1,9 +1,43 @@
 // problem: 160. Intersection of Two Linked Lists
 // difficulty: easy
 // time: 30 minutes
+
+// solution 2
+// runtime: 6ms, beats 23.58%
+// memory: 47.76MB, beats 88.99%
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashMap<ListNode, Integer> map = new HashMap<>();
+
+        while (headA != null) {
+            map.put(headA, 1);
+            headA = headA.next;
+        }
+
+        while (headB != null) {
+            if (map.containsKey(headB)) return headB;
+            headB = headB.next;
+        }
+
+        return null;
+    }
+}
+
+
+// solution 1
 // runtime: 8ms, beats 12.82%
 // memory: 47.92MB, beats 82.84%
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
