8c8ee78ade5580536414674b011300772b36b997
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-08-27 00:10:29 +0500

    Solved easy LeetCode problem LC28.java | String, String Matching, Two Pointers | 27Aug24

---
+++ LC28.java
@@ -0,0 +1,11 @@
+// problem: 28. Find the First Occurence in a String
+// difficulty: easy
+// time: 5 minutes
+// runtime: 0ms, beats 100%
+// memory: 41.08MB, beats 95.24%
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
