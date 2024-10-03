03043f30a2c70437bd0a23dae7e7ed4a85e28996
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-10-03 22:45:47 +0500

    Added optimal solution to LC118.java | 03Oct24

--- LC118.java
+++ LC118.java
@@ -33,3 +33,60 @@ class Solution {
         return list;
     }
 }
+
+
+// 0ms, beats 100%
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        List<Integer> one = new ArrayList<>();
+        one.add(1);
+        list.add(one);
+
+        for (int i=1; i<numRows; i++) {
+            list.add(getPascalRow(i));
+        }
+
+        return list;
+    }
+
+    public static List<Integer> getPascalRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add(nCr(rowIndex, i));
+        }
+        return row;
+    }
+
+    public static int nCr(int n, int r) {
+        if (r==0 || n==r) {
+            return 1;
+        }
+        
+        // int[] A=new int[2*n];
+        // if (A[n]==0) A[n]=fact(n);
+        // if (A[r]==0) A[r]=fact(r);
+        // if (A[n-r]==0) A[n-r]=fact(n-r);
+        int s=1;
+
+        for (int i=0; i<r; i++) {
+            s=s * (n-i) / (i+1);
+        }
+        
+        return s;
+        // return A[n]/A[n-r]*A[r];
+    }
+
+    // public static int fact(int n) {
+    //     if (n<=1) return 1;
+        
+    //     int s=1;
+
+    //     for (int i=2; i<=n; i++) {
+    //         s*=i;
+    //     }
+
+    //     return s;
+    // }
+}
