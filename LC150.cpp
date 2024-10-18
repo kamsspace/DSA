65afa7ae13fde5626f8ff4955c8ae21158c2ace8
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-10-19 00:40:06 +0500

    Updated LeetCode problem LC150.cpp | Array, Stack, Math | 13Oct2024

--- LC150.cpp
+++ LC150.cpp
@@ -36,3 +36,39 @@ public:
         return _stack.top();
     }
 };
+
+
+
+// runtime: 8ms, beats 62%
+// memory: 95%
+class Solution {
+public:
+    int operation(int &a, int &b, string& op) {
+        if (op == "+") return a+b;
+        else if (op == "-") return a-b;
+        else if (op == "*") return a*b;
+        else return a/b;
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        int n = tokens.size();
+        int _stack[n];
+        const string ops = "+-/*";
+        int first=201,second=201,last=-1;
+
+        for (int i=0;i<n;i++) {
+            string c = tokens[i];
+
+            if (ops.find(c) == string::npos) {
+                _stack[++last] = std::stoi(c);
+                continue;
+            } else {
+                second = _stack[last--];
+                first = _stack[last--];
+                _stack[++last] = operation(first, second, c);
+            }
+        }
+
+        return _stack[last];
+    }
+};
