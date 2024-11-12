f62751676f5a9824f70dd4995aa04d08e26a5c80
kamsspace <152371010+kamsspace@users.noreply.github.com>
2024-11-12 23:09:35 +0500

    Updated LC202.java | Hashtable, Math, Two Pointers | 12Nov24

--- LC202.java
+++ LC202.java
@@ -32,3 +32,33 @@ class Solution {
         return sum;
     }
 }
+
+
+// runtime: 1ms, beats 73%
+// memory: 53%
+class Solution {
+    public boolean isHappy(int n) {
+        if (n == 1) return true;
+
+        HashSet<Integer> set = new HashSet<>();
+        
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = nextSum(n);
+        }
+
+        return n == 1;
+    }
+
+    public static int nextSum(int pace) {
+        int sum = 0;
+
+        while (pace > 0) {
+            int k = pace % 10;
+            sum += k*k;
+            pace /= 10;
+        }
+
+        return sum;
+    }
+}
