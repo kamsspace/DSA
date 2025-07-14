ae8fba7381b44878e61ea8c34e84e45da301e42a
Hojiakbar <152371010+kamsspace@users.noreply.github.com>
2025-07-14 07:47:59 +0500

    Updated LC1290.java | Linked List, Math | 14Jul2025

--- LC1290.java
+++ LC1290.java
@@ -1,9 +1,36 @@
-// problem: 1290. Convert Binary Number in a Linked List to Integer.
+// LeetCode problem: 1290. Convert Binary Number in a Linked List to Integer
 // difficulty: easy
-// time: 20 minutes
-// runtime: 0ms, beats 100%
-// memory: 40.67MB, beats 93.35%
+// time: O(n)
+// space: O(1)
 
+// VERSION 2
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int r = 0;
+
+        while (head != null) {
+            r <<= 1;
+            if (head.val == 1) {
+                r |= 1;
+            }
+            head = head.next;
+        }
+
+        return r;
+    }
+}
+
+// VERSION 1
 /**
  * Definition for singly-linked list.
  * public class ListNode {
